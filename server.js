1430bae6fc260abf65165bacf58e4c79c88152f0
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-07 20:07:46 +0200

    backend base finished

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-import express from 'express';
+const app = require('express');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+	cors: {
+		origins: ['http://localhost:3378']
+	}
+});
 
-const app = express();
 const port = 3377;
 
 app.get('/', (req, res) => {
 	res.send('<h1>Socket.io backend</h1>')
 })
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+	console.log('a user connected');
+	socket.on('disconnect', () => {
+		clg('user disconnected');
+	})
+});
+
+express.listen(port, () => {
 	console.log(`listening on port http://localhost:${port}`);
 })
 
